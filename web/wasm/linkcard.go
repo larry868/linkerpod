308561f528f1a45d1555a2261691b6b7ec8c3aa2
lolorenzo777 <lolorenzo777@sunraylab.net>
2023-07-16 18:07:59 +0200

    feat: basic rendering linkerpod.yaml

---
+++ web/wasm/linkcard.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"io"
+	"net/url"
+
+	"github.com/icecake-framework/icecake/pkg/ick"
+	"github.com/icecake-framework/icecake/pkg/ickcore"
+)
+
+type LinkCardSnippet struct {
+	ickcore.BareSnippet
+
+	Name string   // link card name, must be unique
+	HRef *url.URL // URL link card
+}
+
+// Ensuring LinkCardSnippet implements the right interface
+var _ ickcore.ContentComposer = (*LinkCardSnippet)(nil)
+var _ ickcore.TagBuilder = (*LinkCardSnippet)(nil)
+
+func LinkCard(name string) *LinkCardSnippet {
+	n := new(LinkCardSnippet)
+	n.Name = name
+	return n
+}
+
+// ParseHRef tries to parse rawUrl to HRef ignoring error.
+func (lnk *LinkCardSnippet) ParseHRef(rawUrl string) *LinkCardSnippet {
+	lnk.HRef, _ = url.Parse(rawUrl)
+	return lnk
+}
+
+// SetHRef sets the href url
+func (lnk *LinkCardSnippet) SetHRef(href *url.URL) *LinkCardSnippet {
+	if href == nil {
+		lnk.HRef = nil
+	} else {
+		h := *href
+		lnk.HRef = &h
+	}
+	return lnk
+}
+
+/******************************************************************************/
+
+// BuildTag builds the tag used to render the html element.
+func (lnk *LinkCardSnippet) BuildTag() ickcore.Tag {
+	lnk.Tag().SetTagName("div").AddClass("card", "box")
+	return *lnk.Tag()
+}
+
+// RenderContent writes the HTML string corresponding to the content of the HTML element.
+// The default implementation for an HTMLSnippet snippet is to render all the internal stack of composers inside an enclosed HTML tag.
+func (card *LinkCardSnippet) RenderContent(out io.Writer) error {
+	l := ick.Link(ickcore.ToHTML(card.Name)).SetHRef(card.HRef)
+	l.Tag().NoName = true
+	ickcore.RenderChild(out, card, l)
+	return nil
+}
--- web/wasm/webapp.go
+++ web/wasm/webapp.go
@@ -6,7 +6,13 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/icecake-framework/icecake/pkg/dom"
+	"gopkg.in/yaml.v3"
 )
 
 // The main func is required by the wasm GO builder.
@@ -16,7 +22,61 @@ func main() {
 	c := make(chan struct{})
 	fmt.Println("Go/WASM loaded.")
 
+	lmap := make(map[string]*LinkCardSnippet)
+	ys, err := DownloadYaml("linkerpod.yaml")
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		for k, l := range ys.Links {
+			lmap[k] = LinkCard(k).ParseHRef(l.Link)
+		}
+	}
+
+	app := dom.Id("app")
+	for _, l := range lmap {
+		app.InsertSnippet(dom.INSERT_LAST_CHILD, l)
+	}
+
 	// let's go
 	fmt.Println("Go/WASM listening browser events")
 	<-c
+}
+
+type YamlLinkEntry struct {
+	Link string `yaml:"link"`
+}
+
+type YamlStruct struct {
+	Links map[string]YamlLinkEntry `yaml:"links"`
+}
+
+func DownloadYaml(url string) (*YamlStruct, error) {
+	buf := &bytes.Buffer{}
+	err := DownloadFile(buf, url)
+	if err != nil {
+		return nil, fmt.Errorf("DownloadYaml: %+w", err)
+	}
+
+	ys := &YamlStruct{}
+	err = yaml.Unmarshal(buf.Bytes(), ys)
+	if err != nil {
+		return nil, fmt.Errorf("DownloadYaml: %+w", err)
+	}
+
+	//fmt.Println("YAML=> %+v", ys)
+
+	return ys, nil
+}
+
+func DownloadFile(w io.Writer, url string) error {
+	// Get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Write the body to the writer
+	_, err = io.Copy(w, resp.Body)
+	return err
 }
